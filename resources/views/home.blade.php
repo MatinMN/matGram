baae11b71f3165ed44433aaf21c7acff46ac58c9
matin <matin.mn@hotmail.com>
2020-08-09 13:44:01 +0800

    add front-end stubs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,26 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img class="rounded-circle" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRAFrQ8xUDyhaG8mFZzOr8FJg3I-lcZPYa4VQ&usqp=CAU" />
         </div>
+        <div class="col-9 pt-5">
+            <div><h1>MatinMN</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>24</strong> posts</div>
+                <div class="pr-5"><strong>54</strong> followers</div>
+                <div class="pr-5"><strong>68</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">matinmn.com</div>
+            <div>Nunc massa nisl, vestibulum ut nisi vehicula, bibendum scelerisque nisl. Pellentesque vitae nibh sit amet augue finibus elementum. Vivamus consecte.</div>
+            <div><a href="#">www.google.com</a></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4"><img src="https://images.unsplash.com/photo-1542362567-b07e54358753?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="" class="w-100"></div>
+        <div class="col-4"><img src="https://images.pexels.com/photos/241316/pexels-photo-241316.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" class="w-100"></div>
+        <div class="col-4"><img src="https://cdn.autoportal.com/img/new-cars-gallery/renault/kwid/colors/403a91ded24bd7ca07213b427ba1b0a6.jpg" alt="" class="w-100"></div>
     </div>
 </div>
 @endsection
